276f8af2faefe0620cf6f4f3e50aa205b3d27bdc
= <intanmeilanienugraha@gmail.com>
2023-04-23 11:44:34 +0700

    Fix Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <section className={`mx-0.5 mb-0.5 rounded-b-lg bg-red-600`}>
+    <section className="mx-0.5 mb-0.5 rounded-b-lg bg-red-600">
       <div className="px-5 py-5 text-center text-[130%] font-semibold leading-[100%] text-white sm:text-[155%] md:text-[1.875rem] md:leading-[2.25rem]">
         <h1>{`WE'RE LOOKING FORWARD TO HEARING FROM YOU!`}</h1>
       </div>
